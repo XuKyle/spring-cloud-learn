4bc2cbf190d032eb920bca89fe323259049e1f69
kyle <Kyle.Xu@nielsenccdata.com>
2017-09-05 16:30:26 +0800

    1、zuul

---
+++ service-zuul/src/main/java/com/kyle/servicezuul/ServiceZuulApplication.java
@@ -0,0 +1,17 @@
+package com.kyle.servicezuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+/*zuul.1:开启zuul功能*/
+@EnableZuulProxy
+@EnableEurekaClient
+public class ServiceZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceZuulApplication.class, args);
+    }
+}
